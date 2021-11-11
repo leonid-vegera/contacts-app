f15a58438a62cb13df21afca9b1d9f55b9c7c7c3
Leonid Vegera <leonid.vegera@gmail.com>
2021-11-11 21:45:55 +0200

    Updates

--- static/js/main.6876289d.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpcontacts-app"]=this["webpackJsonpcontacts-app"]||[]).push([[0],{9:function(t,c,e){"use strict";e.r(c);e(1);var n=e(3),s=e.n(n),a=e(0);var p=function(){return Object(a.jsx)("div",{className:"App",children:Object(a.jsx)("h1",{children:"Contacts"})})};s.a.render(Object(a.jsx)(p,{}),document.getElementById("root"))}},[[9,1,2]]]);
-//# sourceMappingURL=main.6876289d.chunk.js.map
